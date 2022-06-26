eb136a5d73ce1f9c26a799c1ebd9aac55f96c934
isailara <isailara20002@gmail.com>
2022-06-25 21:15:45 -0500

    help

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,39 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
+        File file = new File("archivo.txt");
+
+        try {
+            boolean fileCreated=file.createNewFile();
+
+            FileWriter fileWriter=new FileWriter(file);
+            fileWriter.write("Real Good Feeling");
+            fileWriter.close();
+
+            if(fileCreated){
+                System.out.println("El archivo se creó");
+            }
+            else{
+                System.out.println("El archivo ya existe");
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        //Lectura del archivo
+        try {
+            Scanner scanner=new Scanner(file);
+            while (scanner.hasNextLine()){
+                String data=scanner.nextLine();
+                System.out.println(data);
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
